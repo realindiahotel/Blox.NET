b04daed5a24fbecfeae42c4eefb7bb39c73e107f
Thashiznets <thashiznets@yahoo.com.au>
2015-02-20 16:20:32 +1100

    Added DB_PWD.txt to the gitignore

--- Lego.NET/Data/DatabaseConnection.cs
+++ Lego.NET/Data/DatabaseConnection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Bitcoin.Lego.Data
 {
@@ -10,6 +11,19 @@ namespace Bitcoin.Lego.Data
 	{
 		private static String _superUserLegoDbPwd;
 		private static String _connectionString = @"Server=tcp:cglpto7ask.database.windows.net,1433;Database=Lego.NET.DB;User ID=SuperUserLego@cglpto7ask;Password="+_superUserLegoDbPwd+@"Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
-       
+
+		static DatabaseConnection()
+		{
+			try
+			{
+				TextReader tr = new StreamReader(@"\Data\DB_PWD.txt");
+				_superUserLegoDbPwd = tr.ReadLine();
+				tr.Close();
+			}
+			catch
+			{
+				_superUserLegoDbPwd = "";
+			}
+		}
 	}
 }
