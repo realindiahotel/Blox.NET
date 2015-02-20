890748a30aef1e79825dba96ea32e92f0d36b794
Thashiznets <thashiznets@yahoo.com.au>
2015-02-20 16:01:39 +1100

    Starting DB Wiring

---
+++ Lego.NET/Data/DatabaseConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitcoin.Lego.Data
+{
+	public static class DatabaseConnection
+	{
+		private static String _superUserLegoDbPwd;
+		private static String _connectionString = @"Server=tcp:cglpto7ask.database.windows.net,1433;Database=Lego.NET.DB;User ID=SuperUserLego@cglpto7ask;Password="+_superUserLegoDbPwd+@"Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+       
+	}
+}
